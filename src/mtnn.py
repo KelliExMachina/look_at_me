df48552038b90c0c69934d419a60713553551e0d
Kelli Kennedy <kelliexmachina@gmail.com>
2020-04-13 20:18:50 -0500

    Create directory structure, add dummies

---
+++ src/mtnn.py
@@ -0,0 +1,13 @@
+# Standard Imports
+import numpy as numpy
+import pandas as pd
+
+
+# Visualizations
+import plotly.graph_objects as go
+
+# PNN - face classification
+
+# RNN - bounding box regression
+
+# ONN - facial landmark localization
